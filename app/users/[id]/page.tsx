df835ba8d9fdf238f51f912154d4fa23c2f97460
Yutian Lei <yutian.lei@uzh.ch>
2025-03-29 23:11:30 +0100

    page that displays when accessing /users/[id]

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -4,20 +4,16 @@
 // import { Card } from "antd"; // similar to /app/users/page.tsx
 
 "use client";
-// For components that need React hooks and browser APIs,
-// SSR (server side rendering) has to be disabled.
-// Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 
 import React from "react";
+import UserProfilePage from "@/components/profile/UserProfilePage";
 
-const Profile: React.FC = () => {
-  return (
-    <div className="card-container">
-      <p>
-        <strong>SampleUser</strong>
-      </p>
-    </div>
-  );
+/**
+ * Page component that renders the user profile
+ * This is the page that displays when accessing /users/[id]
+ */
+const ProfilePage: React.FC = () => {
+  return <UserProfilePage />;
 };
 
-export default Profile;
+export default ProfilePage;
