ad0b244d5d0ebb9dd9e71a13e6f5d1aa6a0edd74
Maorong Lin <spares-boxers.0k@icloud.com>
2025-04-16 19:20:13 +0200

    implemented rules and tutorials

---
+++ app/tutorial/page.tsx
@@ -0,0 +1,9 @@
+import RulesAndTutorials from '@/components/RulesAndTutorials/RulesAndTutorials';
+
+export default function RulesPage() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-8">
+      <RulesAndTutorials />
+    </main>
+  );
+}
